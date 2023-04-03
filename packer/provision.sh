ee76ac4e88f92128667a0164c319424fabdac0fd
patoarvizu <patoarvizu@gmail.com>
2023-04-03 12:06:05 -0400

    Trim the Packer provisioning script a little bit

--- packer/provision.sh
+++ packer/provision.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install unzip curl vim jq dnsutils amazon-ecr-credential-helper -y
+sudo apt install unzip curl vim -y
 
-sudo apt remove docker docker-engine docker.io
 echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) \
-      stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce
-sudo service docker restart
 sudo usermod -aG docker vagrant
-sudo docker --version
 
-curl -sSL -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-amd64-v0.8.6.tgz
+curl -sSL -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-amd64-v1.2.0.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
 
@@ -54,14 +48,12 @@ EOF
 (
 cat <<-EOF
   [Unit]
-  Description=nomad agent
   Requires=network-online.target
-  After=network-online.target
 
   [Service]
   Restart=on-failure
   ExecStart=/usr/bin/nomad agent -config=/etc/nomad.d/server.hcl -config=/etc/nomad.d/bootstrap.hcl
-  ExecReload=/bin/kill -HUP $MAINPID
+  ExecReload=/bin/kill -HUP \$MAINPID
 
   [Install]
   WantedBy=multi-user.target
@@ -92,20 +84,14 @@ EOF
 (
 cat <<-EOF
   [Unit]
-  Description=consul client
   Requires=network-online.target
-  After=network-online.target
 
   [Service]
   Restart=on-failure
   ExecStart=/usr/bin/consul agent -config-file=/etc/consul.d/config.hcl -config-file=/etc/consul.d/join.hcl
-  ExecReload=/bin/kill -HUP $MAINPID
+  ExecReload=/bin/kill -HUP \$MAINPID
 
   [Install]
   WantedBy=multi-user.target
 EOF
 ) | sudo tee /etc/systemd/system/consul.service
-
-echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-arptables
-echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-ip6tables
-echo 1 | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
