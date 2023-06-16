220f872716a708b5323209222fea3b5196c3d746
patoarvizu <patoarvizu@gmail.com>
2023-06-16 15:41:00 -0400

    Remove unnecessary Boundary configurations in Vault

--- 4-configure-vault/locals.tf
+++ 4-configure-vault/locals.tf
@@ -1,8 +1,4 @@
 locals {
-  boundary_user_data_json = {
-    policies = [ "boundary" ]
-    password = "boundary123"
-  }
   vnc_password = {
     password = "idbehold"
   }
--- 4-configure-vault/vault.tf
+++ 4-configure-vault/vault.tf
@@ -1,14 +1,3 @@
-resource vault_auth_backend userpass {
-  type = "userpass"
-}
-
-resource vault_generic_endpoint boundary_user {
-  depends_on = [ vault_auth_backend.userpass ]
-  path = "auth/userpass/users/boundary"
-  ignore_absent_fields = true
-  data_json = jsonencode(local.boundary_user_data_json)
-}
-
 resource vault_policy boundary_controller {
   name = "boundary-controller"
   policy = data.vault_policy_document.boundary_controller.hcl
